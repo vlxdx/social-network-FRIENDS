d5fa7875e1acd518d06bc9e497025542904553cb
vlxdx <volodymyr.dv@protonmail.com>
2023-03-01 12:18:49 +0100

    login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-    <img src='https://toppng.com/uploads/preview/friends-png-11553964505tjuzlefal7.png' />
+        <img src='https://toppng.com/uploads/preview/friends-png-11553964505tjuzlefal7.png' />
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 
