7c665ba63d20e934e9d5c129bdbaddc6ac0ef1fa
vlxdx <volodymyr.dv@protonmail.com>
2023-03-04 09:23:58 +0100

    redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import LoginPage from './components/Login/Login';
 
 const App = () => {
   return (
@@ -15,16 +16,19 @@ const App = () => {
         <div className='app-booker-content'>
           <Routes>
             <Route path='/dialogs'
-              element={<DialogsContainer /> }/>
-            
+              element={<DialogsContainer />} />
+
             <Route path='/profile/:userId'
-              element={<ProfileContainer /> }/>
-              
+              element={<ProfileContainer />} />
+
             <Route path='/profile'
-              element={<ProfileContainer /> }/>
+              element={<ProfileContainer />} />
 
             <Route path='/users'
-              element={<UsersContainer /> }/>
+              element={<UsersContainer />} />
+
+            <Route path='/login'
+              element={<LoginPage />} />
           </Routes>
         </div>
       </div>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import { Navigate } from 'react-router-dom';
 
 const Dialogs = (props) => {
 
@@ -18,6 +19,8 @@ const Dialogs = (props) => {
         let body = e.target.value;
         props.updateNewMessageBody(body);
     }
+
+    if (!props.isAuth) return <Navigate to={"/login"}/>;
 
     return (
         <div className={s.dialogs}>
